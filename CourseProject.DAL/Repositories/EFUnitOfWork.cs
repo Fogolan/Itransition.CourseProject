2cad0a61e024a7b4994cf309e086575713b14b58
Fogolan <Fogolan@gmail.com>
2017-02-23 16:11:25 +0300

    Fixed DAL without authorization

--- CourseProject.DAL/Repositories/EFUnitOfWork.cs
+++ CourseProject.DAL/Repositories/EFUnitOfWork.cs
@@ -8,5 +8,87 @@ namespace CourseProject.DAL.Repositories
     class EFUnitOfWork : IDisposable, IUnitOfWork
     {
         private InstructionContext db;
+        private InstructionRepository instructionRepository;
+        private UserRepository userRepository;
+        private StepRepository stepRepository;
+        private CommentRepository commentRepository;
+        private TagRepository tagRepository;
+
+        public IRepositoryGettable<Instruction> Instructions
+        {
+            get
+            {
+                if (instructionRepository == null)
+                    instructionRepository = new InstructionRepository(db);
+                return instructionRepository;
+            }
+        }
+
+        public IRepositoryGettable<User> Users
+        {
+            get
+            {
+                if (userRepository == null)
+                    userRepository = new UserRepository(db);
+                return userRepository;
+            }
+        }
+
+        public IRepositoryUpdatable<Step> Steps
+        {
+            get
+            {
+                if (stepRepository == null)
+                    stepRepository = new StepRepository(db);
+                return stepRepository;
+            }
+        }
+
+        public IRepository<Comment> Comments
+        {
+            get
+            {
+                if (commentRepository == null)
+                    commentRepository = new CommentRepository(db);
+                return commentRepository;
+            }
+        }
+
+        public IRepository<Tag> Tags
+        {
+            get
+            {
+                if (tagRepository == null)
+                    tagRepository = new TagRepository(db);
+                return tagRepository;
+            }
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
